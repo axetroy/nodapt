ce8f7f61e9967e4947c3e6754b110748cc975943
Axetroy <axetroy.dev@gmail.com>
2024-09-25 03:15:51 +0800

    refactor: remove unused Config struct and LoadConfig function from cli.go

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,12 +1,10 @@
 package cli
 
 import (
-	"encoding/json"
 	"os"
 	"path/filepath"
 
 	"github.com/axetroy/virtual_node_env/internal/util"
-	"github.com/pkg/errors"
 )
 
 var virtual_node_env_dir string
@@ -27,28 +25,4 @@ func init() {
 	}
 
 	virtual_node_env_dir = filepath.Join(homeDir, ".virtual-node-env")
-}
-
-type Config struct {
-	Node string `json:"node"`
-}
-
-func LoadConfig(filePath string) (*Config, error) {
-	content, err := os.ReadFile(filePath)
-
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil, errors.WithMessage(err, "Can not found `.virtual-node-env.json` file.")
-		}
-
-		return nil, errors.WithStack(err)
-	}
-
-	c := &Config{}
-
-	if err := json.Unmarshal(content, c); err != nil {
-		return nil, errors.WithMessagef(err, "Read config file %s failed", filePath)
-	}
-
-	return c, nil
 }
