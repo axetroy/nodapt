4eb0ef005820c2666469ada577f90181de61606c
Axetroy <axetroy.dev@gmail.com>
2024-10-10 22:46:04 +0800

    Refactor appendEnvPath to include pathDir at both the beginning and end of the PATH environment variable

--- internal/util/appendEnvPath.go
+++ internal/util/appendEnvPath.go
@@ -8,7 +8,7 @@ import (
 func AppendEnvPath(pathDir string) string {
 	oldPath := os.Getenv("PATH")
 
-	newPath := fmt.Sprintf("%s%c%s", pathDir, os.PathListSeparator, oldPath)
+	newPath := fmt.Sprintf("%s%c%s%c%s", pathDir, os.PathListSeparator, oldPath, os.PathListSeparator, pathDir)
 
 	return newPath
 }
