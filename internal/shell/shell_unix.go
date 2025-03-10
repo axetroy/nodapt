35f5a40d4af5f45e21b77dbc95673429ad0665c1
Axetroy <axetroy.dev@gmail.com>
2025-03-11 00:53:02 +0800

    增强 shell 识别功能，添加获取父进程信息的逻辑以支持已知 shell 的检测

--- internal/shell/shell_unix.go
+++ internal/shell/shell_unix.go
@@ -3,9 +3,12 @@
 package shell
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
+	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -21,22 +24,73 @@ func getShellFromEnv() *string {
 	return &shell
 }
 
-func getShellFromProcess() (string, error) {
-	out, err := exec.Command("ps", "-p", fmt.Sprint(os.Getppid()), "-o", "comm=").Output()
+var knownShells = []string{"sh", "bash", "zsh", "fish", "dash", "ash", "ksh"}
 
+func isKnownShell(shellPath string) bool {
+	parts := strings.Split(shellPath, "/")
+
+	executableFileName := parts[len(parts)-1]
+
+	return slices.Contains(knownShells, executableFileName)
+}
+
+func getParentProcessInfo(pid int) (int, string, error) {
+	cmd := exec.Command("ps", "-o", "ppid,comm", "-p", fmt.Sprintf("%d", pid))
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
 	if err != nil {
-		return "", errors.WithStack(err)
+		return -1, "", err
 	}
 
-	shellName := strings.TrimSpace(string(out))
+	lines := strings.Split(out.String(), "\n")
+	if len(lines) < 2 {
+		return -1, "", fmt.Errorf("no output from ps")
+	}
 
-	return shellName, nil
+	fields := strings.Fields(lines[1])
+	if len(fields) < 2 {
+		return -1, "", fmt.Errorf("unexpected output format")
+	}
+
+	ppid := fields[0]
+	command := fields[1]
+
+	ppidInt, err := strconv.Atoi(ppid)
+
+	if err != nil {
+		return -1, "", err
+	}
+
+	return ppidInt, command, nil
+}
+
+func getShellFromProcess(pid int) (string, error) {
+	for {
+		ppid, command, err := getParentProcessInfo(pid)
+
+		if err != nil {
+			return "", err
+		}
+
+		if isKnownShell(command) {
+			return command, nil
+		}
+
+		pid = ppid
+	}
+
 }
 
 func GetPath() (string, error) {
+	if shell, err := getShellFromProcess(os.Getppid()); err == nil {
+		return shell, nil
+	}
+
 	if shell := getShellFromEnv(); shell != nil {
 		return *shell, nil
 	}
 
-	return getShellFromProcess()
+	return "", errors.New("can't detect shell")
 }
