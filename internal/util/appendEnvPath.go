3006ca242872eeeaf847d157c0e4ec2d6974a27f
Axetroy <axetroy.dev@gmail.com>
2025-04-13 23:38:01 +0800

    重构 PATH 处理逻辑，添加 removeNodePath 函数以移除包含 node 二进制文件的目录, close #12

--- internal/util/appendEnvPath.go
+++ internal/util/appendEnvPath.go
@@ -3,10 +3,59 @@ package util
 import (
 	"fmt"
 	"os"
+	"runtime"
+	"strings"
 )
 
+func removeFromSlice(s []string, r string) []string {
+	result := s[:0] // Use the same slice to avoid extra allocations
+	for _, v := range s {
+		if v != r {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func removeNodePath(paths string) string {
+	// Split the PATH into directories
+	dirs := strings.Split(paths, string(os.PathListSeparator))
+
+	// Remove the node binary directory from the PATH
+	for _, dir := range dirs {
+		stat, err := os.Stat(dir)
+
+		if err != nil {
+			continue
+		}
+
+		if !stat.IsDir() {
+			continue
+		}
+
+		// Check if the directory contains the node binary
+		files, err := os.ReadDir(dir)
+		if err != nil {
+			continue
+		}
+
+	fileLoop:
+		for _, file := range files {
+			isContainsNodeBinary := (runtime.GOOS == "windows" && file.Name() == "node.exe") || (runtime.GOOS != "windows" && file.Name() == "node")
+
+			if isContainsNodeBinary {
+				// Remove the directory from the PATH
+				dirs = removeFromSlice(dirs, dir)
+				break fileLoop
+			}
+		}
+	}
+
+	return strings.Join(dirs, string(os.PathListSeparator))
+}
+
 func AppendEnvPath(pathDir string) string {
-	oldPath := os.Getenv("PATH")
+	oldPath := removeNodePath(os.Getenv("PATH"))
 
 	newPath := fmt.Sprintf("%s%c%s%c%s", pathDir, os.PathListSeparator, oldPath, os.PathListSeparator, pathDir)
 
